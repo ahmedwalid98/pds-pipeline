2c303e687e60c6777263cde829efc2880d0a2842
Ahmed Walid <ahmedawlid98.aw@gmail.com>
2026-06-04 19:46:30 +0300

    feat: added the import_csv function to the entry app main

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from backup.rename import rename_to_zip
 from backup.extract import unzip_backup
 from paradox.reader import reade_tables
 from paradox.export import extracting_csv
+from database.importer import import_csv
 import glob
 import os
 import time
 import logging
 import sys
+from database.create_db import create_database
 
 logging.basicConfig(
     level=logging.INFO,
@@ -18,8 +20,8 @@ logging.basicConfig(
 )
 log = logging.getLogger(__name__)
 
-paradox_path = r"D:\Conversion\St. Jude (Clinton)"
-output_csv = r"D:\ExportedCSVs"
+paradox_path = r"D:\Conversion\St. Henry (Hardin)"
+output_csv = r"D:\Conversion\St. Henry (Hardin)\CSV_silver"
 
 
 backup = glob.glob(os.path.join(paradox_path, "*.pds"))
@@ -37,7 +39,7 @@ start = time.perf_counter()
 
 log.info("Step 1/4 — Renaming .pds → .zip ...")
 zip_file = rename_to_zip(pds_file)
-log.info("  ZIP: %s", zip_file)
+log.info("ZIP: %s", zip_file)
 
 log.info("Step 2/4 — Extracting archive ...")
 extract_path = unzip_backup(zip_file, paradox_path)
@@ -49,7 +51,11 @@ db_files = reade_tables(extract_path)
 log.info("Step 4/4 — Exporting CSV ...")
 output_csv = extracting_csv(db_files, output_csv)
 log.info(f"CSV files exported to {output_csv}")
-print(output_csv)
 
-end = time.time()
-print(f"\n🕒 Finished in {end - start:.2f} seconds")
+log.info("Creating SQLite Database")
+conn = create_database(pds_file)
+
+log.info("Importin CSV Into SQLite Database")
+import_csv(output_csv, conn)
+end = time.perf_counter()
+log.info(f"\n🕒 Finished in {end - start:.2f} seconds")
